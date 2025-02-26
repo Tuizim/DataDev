056913a84e43093f5acee90f1c96d97d7b0ed16b
Tuizim <arturtrombim.dev@gmail.com>
2025-02-26 09:40:25 -0300

    [FEATURE] Função de endereço, conectado a um banco local

--- src/main.js
+++ src/main.js
@@ -1,15 +1,20 @@
-const {app, BrowserWindow} = require('electron');
+const { app, BrowserWindow } = require('electron');
 const path = require('path');
 let mainWindow;
 
-app.whenReady().then(()=>{
+app.whenReady().then(() => {
     mainWindow = new BrowserWindow({
-        width:1000,
-        height:700,
+        width: 1000,
+        height: 700,
         resizable: false,
-        icon: path.join(__dirname,'assets','ico.png')
-        
+        icon: path.join(__dirname, 'assets', 'ico.png'),
+        webPreferences: {
+            preload: path.join(__dirname, './preload.js'),
+            contextIsolation: true,
+            enableRemoteModule: false,
+            nodeIntegration: true,
+          }
     });
-    mainWindow.removeMenu()
-    mainWindow.loadURL(path.join(__dirname,"main",'index.html'))
+    mainWindow.menuBarVisible = false;
+    mainWindow.loadURL(path.join(__dirname, "main", 'index.html'));
 });
---
+++ src/preload.js
@@ -0,0 +1,23 @@
+const { contextBridge } = require('electron');
+const sqlite3 = require('sqlite3').verbose();
+const path = require('path');
+
+const db = new sqlite3.Database(path.join(__dirname, 'assets', 'db', 'enderecos.db'), (err) => {
+  if (err) {
+    console.error('Erro ao abrir o banco de dados:', err.message);
+  } else {
+    console.log('Conectado ao banco de dados SQLite.');
+  }
+});
+
+contextBridge.exposeInMainWorld('database', {
+  gerarEndereco: () => new Promise((resolve, reject) => {
+    db.get("SELECT * FROM enderecos ORDER BY RANDOM() LIMIT 1", (err, row) => {
+      if (err) {
+        reject(err.message);
+      } else {
+        resolve(row);
+      }
+    });
+  }),
+});
